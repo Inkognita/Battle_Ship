a0bdcc5aac8ae21a9a1486f9412a4b5e86a91f0c
Inkognita <liepieshov@ucu.edu.ua>
2017-02-07 14:20:11 +0200

    rOSTIK SASAY

--- script.py
+++ script.py
@@ -1,9 +1,41 @@
-def main():
-    '''
-    LOL
-    :return:
-     Pisya
-    '''
-    print("Hello!")
+def read_file(filename):
+    """
+    (str) -> (data)
+    """
 
-main()
+    pass
+
+
+def has_ship():
+    """
+    (data, tuple) -> (bool)
+    """
+    pass
+
+
+def ship_size():
+    """
+    (data, tuple) -> (tuple)
+    """
+    pass
+
+
+def is_valid():
+    """
+    (data) -> (bool)
+    """
+    pass
+
+
+def field_to_str():
+    """
+    (data) -> (str)
+    """
+    pass
+
+
+def generate_field():
+    """
+    () -> (data)
+    """
+    pass
